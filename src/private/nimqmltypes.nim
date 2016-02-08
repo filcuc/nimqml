b3c8cbe3367bbdf12f2ed6bbf13c80cfa82d1828
Filippo Cucchetto <filippocucchetto@gmail.com>
2016-02-09 00:12:02 +0100

    Added missing types

--- src/private/nimqmltypes.nim
+++ src/private/nimqmltypes.nim
@@ -1,27 +1,62 @@
 type
-  QObject* = ref object of RootObj
+  QObject* = ref object of RootObj ## \
+    ## A QObject
     vptr: DosQObject
 
-  QVariant* = ref object of RootObj
+  QVariant* = ref object of RootObj ## \
+    ## A QVariant
     vptr: DosQVariant
 
-  QMetaType* = cint
+  QtItemFlag* {.pure.} = enum ## \
+    ## Item flags
+    ##
+    ## This enum mimic the Qt::itemFlag C++ enum
+    None = 0.cint,
+    IsSelectable = 1.cint,
+    IsEditable = 2.cint,
+    IsDragEnabled = 4.cint,
+    IsDropEnabled = 8.cint,
+    IsUserCheckable = 16.cint,
+    IsEnabled = 32.cint,
+    IsTristate = 64.cint,
+    NeverHasChildren = 128.cint
+
+  QtOrientation* {.pure.} = enum ## \
+    ## Define orientation
+    ##
+    ## This enum mimic the Qt::Orientation C++ enum
+    Horizontal = 1.cint,
+    Vertical = 2.cint
+
+  QMetaType* {.pure.} = enum ## \
+    ## Qt metatypes values used for specifing the
+    ## signals and slots argument and return types.
+    ##
+    ## This enum mimic the QMetaType::Type C++ enum
+    UnknownType = 0.cint,
+    Bool = 1.cint,
+    Int = 2.cint,
+    QString = 10.cint,
+    VoidStar = 31.cint,
+    QObjectStar = 39.cint,
+    QVariant = 41.cint,
+    Void = 43.cint,
 
   SignalDefinition* = object
-    name: string
-    parametersTypes: seq[QMetaType]
+    name*: string
+    parametersTypes*: seq[QMetaType]
 
   SlotDefinition* = object
-    name: string
-    returnMetaType: QMetaType
-    parametersTypes: seq[QMetaType]
+    name*: string
+    returnMetaType*: QMetaType
+    parametersTypes*: seq[QMetaType]
 
   PropertyDefinition* = object
-    name: string
-    propertyMetaType: QMetaType
-    readSlot: string
-    writeSlot: string
-    notifySignal: string
+    name*: string
+    propertyMetaType*: QMetaType
+    readSlot*: string
+    writeSlot*: string
+    notifySignal*: string
 
   QMetaObject* = ref object of RootObj
     vptr: DosQMetaObject
