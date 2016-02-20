e31df596201e8e1a2d0bacbc66cfef8caaf459a9
Filippo Cucchetto <filippocucchetto@gmail.com>
2016-02-20 11:03:13 +0100

    Added missing exports for QUrl

--- src/private/qurl.nim
+++ src/private/qurl.nim
@@ -2,7 +2,7 @@ proc setup*(self: QUrl, url: string, mode: QUrlParsingMode) =
   ## Setup a new QUrl
   dos_qurl_create(self.vptr, url.cstring, mode.cint)
 
-proc delete(self: QUrl) =
+proc delete*(self: QUrl) =
   ## Delete a QUrl
   if self.vptr.isNil:
     return
@@ -10,12 +10,12 @@ proc delete(self: QUrl) =
   dos_qurl_delete(self.vptr)
   self.vptr.resetToNil
 
-proc newQUrl(url: string, mode: QUrlParsingMode = QUrlParsingMode.Tolerant): QUrl =
+proc newQUrl*(url: string, mode: QUrlParsingMode = QUrlParsingMode.Tolerant): QUrl =
   ## Create a new QUrl
   new(result, delete)
   result.setup(url, mode)
 
-proc toString(self: QUrl): string =
+proc toString*(self: QUrl): string =
   ## Return the url string
   var str: cstring
   dos_qurl_to_string(self.vptr, str)
